fac970c4587b0f0a45cd88bd5929002706b25e97
MartinGruter <Martin.Grter@iths.se>
2026-05-19 10:04:04 +0200

    Init

---
+++ src/main/java/se/iths/martin/emailserviceprojekt2/EmailServiceProjekt2Application.java
@@ -0,0 +1,13 @@
+package se.iths.martin.emailserviceprojekt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceProjekt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceProjekt2Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/martin/emailserviceprojekt2/EmailServiceProjekt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.martin.emailserviceprojekt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceProjekt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
