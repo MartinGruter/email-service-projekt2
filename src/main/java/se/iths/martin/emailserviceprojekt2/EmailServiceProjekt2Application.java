168d8fb780806e370bd8dfeead9932a4817d80e8
MartinGruter <Martin.Grter@iths.se>
2026-05-26 13:12:27 +0200

    Added Message, MessageSubscriber and did changes in properties, pom and main.

--- src/main/java/se/iths/martin/emailserviceprojekt2/EmailServiceProjekt2Application.java
+++ src/main/java/se/iths/martin/emailserviceprojekt2/EmailServiceProjekt2Application.java
@@ -3,7 +3,9 @@ package se.iths.martin.emailserviceprojekt2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "se.iths.martin"
+})
 public class EmailServiceProjekt2Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/se/iths/martin/emailserviceprojekt2/model/Message.java
@@ -0,0 +1,15 @@
+package se.iths.martin.emailserviceprojekt2.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class Message implements Serializable {
+    private String content;
+    private String sender;
+    private LocalDateTime timestamp;
+}
